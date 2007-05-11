ef433ec5f86e8fb0bc4fe49b570cf08342ee3825
kappa <kappa@kapecod.kapranoff.ru>
2007-05-12 03:39:49 +0400

    .

--- suslikd.pl
+++ suslikd.pl
@@ -5,11 +5,16 @@ use warnings;
 package Suslik::Daemon;
 use base 'Net::Server::PreFork';
 
+use DBI;
+use Encode;
+
 my $TIMEOUT = 30;
 my $HOST = 'kapecod';
 my $PORT = 70;
+my $WALL_DB = "$ENV{HOME}/work/gopher/wall_db";
 
 my $CRLF = "\x0D\x0A";
+my $outer_encoding = 'windows-1251';
 
 sub goph($);
 sub gophinp($$);
@@ -30,7 +35,7 @@ sub process_request {
         my ($selector, $query) = split("\t", $str);
         $selector ||= q{}; $query ||= q{};
 
-        $self->dispatch($selector, $query);
+        $self->dispatch($selector, Encode::decode('utf-8', $query));
 
         alarm($prev_alarm);
     };
@@ -42,13 +47,23 @@ sub process_request {
 
 our %action = (
     default         => sub {
+        if ($_[1]) {
+            write_wall(@_);
+        }
+
         goph 'Привет!';
-        goph '
-        gophinp 'Сказать что-нибудь...' => 'say';
-        gophend;
-    },
-    say             => sub {
-        goph "Вы сказали: [$_[0]]";
+        goph 'Будет лучше, если Вы переключитесь в windows-1251.';
+        goph 'На всякий случай для тех, кто пока не видит русских букв, повторю 4 раза:';
+        goph 'windows-1251, windows-1251, windows-1251, windows-1251!';
+        goph '';
+        goph 'Это Стена, на ней пишут буквы.';
+        goph '';
+        goph 'Вот смотрите:';
+        foreach (read_wall()) {
+            goph $_;
+        }
+        goph '';
+        gophinp 'Написать ещё букв...' => '';
         gophend;
     },
 );
@@ -56,7 +71,7 @@ our %action = (
 sub dispatch {
     my ($self, $selector, $query) = @_;
 
-    $action{exists $action{$selector} ? $selector : 'default'}->($query);
+    $action{exists $action{$selector} ? $selector : 'default'}->($self, $query);
 }
 
 sub goph($) {
@@ -65,8 +80,10 @@ sub goph($) {
     print "i$str\t%\terror.host\t$PORT$CRLF";
 }
 
-sub gophend {
-    goph ' -- powered by suslikd.pl,v$Id$';
+sub gophend() {
+
+    goph '' for 1 .. 5;
+    goph ' -- powered by suslikd.pl';
     print ".$CRLF";
 }
 
@@ -76,7 +93,34 @@ sub gophinp($$) {
     print "7$prompt\t$action\t$HOST\t$PORT$CRLF";
 }
 
+sub read_wall {
+    my $dbh = DBI->connect("dbi:SQLite:dbname=$WALL_DB", q{}, q{});
+
+    my @wall = @{$dbh->selectall_arrayref("select time, who, words from wall order by time")};
+
+    return map {
+        my ($time, $who, $words) = @{$_};
+        $who =~ /(\w+\.\w+)$/
+            and $who = $1;
+
+#        localtime($time) . ', кто-то@' . $who . ': ' . $words;
+        'кто-то@' . $who . ': ' . $words;
+    } @wall;
+}
+
+sub write_wall {
+    my ($self, $str) = @_;
+    my $dbh = DBI->connect("dbi:SQLite:dbname=$WALL_DB", q{}, q{});
+
+    $dbh->do("insert into wall values (?, ?, ?)",
+        undef, time(), $self->{server}->{peerhost} || $self->{server}->{peeraddr}, $str);
+}
+
 package main;
 
-Suslik::Daemon->run(port => $PORT);
+Suslik::Daemon->run(
+    port => $PORT,
+
+    reverse_lookups => 1,
+);
 exit;
